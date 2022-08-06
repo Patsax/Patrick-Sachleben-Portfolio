3ac78265cb093b14829f7021193c420211bd000a
Patrick <patasax22@yahoo.com>
2022-08-06 11:47:11 -0400

    project descriptions added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,6 +11,7 @@ const data = [
     id: 1,
     image: img1,
     title: 'H-Booker',
+    description: 'This is an app that shows a few pictures and a description of rooms in a hotel and allows you to book one of them. This was a group project and I was responsible for setting up most of the front end and helped connect to a small database of the room info.',
     github: 'https://github.com/Patsax/h-booker',
     demo: 'https://h-booker.herokuapp.com/'
   },
@@ -18,6 +19,7 @@ const data = [
     id: 2,
     image: img2,
     title: 'Night Picks',
+    description: 'This app takes input from the user and produces suggestions for a movie to watch along with a cocktail to enjoy possibly with a partner. This was a group project and I primarily helped design the front end look.',
     github: 'https://github.com/Patsax/Night-Picks',
     demo: 'https://patsax.github.io/Night-Picks/'
   },
@@ -25,6 +27,7 @@ const data = [
     id: 3,
     image: img3,
     title: 'git-it-done',
+    description: 'This app is an alternative way to search Github. It specifically searches projects with open issues for developers to contribute. You can search for specific users or just by wether the project has HTML, CSS, or JavaScript.',
     github: 'https://github.com/Patsax/git-it-done',
     demo: 'https://patsax.github.io/git-it-done/'
   },
@@ -32,6 +35,7 @@ const data = [
     id: 4,
     image: img4,
     title: 'Workday-Planning',
+    description: 'This app was developed with the 9 to 5 employee in mind. The user can set tasks and events in certain time blocks. As the day passes, the different blocks change colors to indicate status.',
     github: 'https://github.com/Patsax/workday-planning',
     demo: 'https://patsax.github.io/workday-planning/'
   },
@@ -39,6 +43,7 @@ const data = [
     id: 5,
     image: img5,
     title: 'Taskinator',
+    description: 'This is a simple To Do list with drag and drop functionality. The user can just type in a task name and pick a type, then it will appear in the To Do section. The task can then be moved to and from the other sections.',
     github: 'https://github.com/Patsax/taskinator',
     demo: 'https://patsax.github.io/taskinator/'
   }
@@ -52,13 +57,14 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         {
-          data.map(({id, image, title, github, demo}) => {
+          data.map(({id, image, title, description, github, demo}) => {
             return (
               <article id={id} className='portfolio__item'>
                 <div className="portfolio__item-img">
                   <img src={image} alt={title} />
                 </div>
                 <h3>{title}</h3>
+                <p>{description}</p>
                 <div className="portfolio__item-cta">
                   <a href={github} className='btn' target='_blank' rel='noreferrer'>Github</a>
                   <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer'>Live Demo</a>
